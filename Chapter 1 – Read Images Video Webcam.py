63d746a235f70ca8cd91412d1553ad6555e09d2b
Dương Văn Hiệp <129377698+imdhiep@users.noreply.github.com>
2024-05-26 00:37:50 +0700

    Update Chapter 1 – Read Images Video Webcam.py

--- Chapter 1 – Read Images Video Webcam.py
+++ Chapter 1 – Read Images Video Webcam.py
@@ -1,39 +1,24 @@
 import cv2
-""" print("Package Imported")
 
-image = cv2.imread("Resources/lambo.png")
-
-cv2.imshow("Output" ,image)
-cv2.waitKey(0) """
-
-"""cap = cv2.VideoCapture(1)
-cap.set(3,640)
-cap.set(4,480)
-cap.set(10,100)
-
-while True:
-    success, img = cap.read()
-    cv2.imshow("Result",img)
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
-        break"""
-        
+# Đọc ảnh từ tệp và hiển thị
 img = cv2.imread("Resources/lena.png")
-imgGray = cv2.cvtColor(img,cv2.COLOR_BAYER_BG2GRAY)
-
+imgGray = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2GRAY)
 cv2.imshow("Gray Image", imgGray)
 cv2.waitKey(0)
 
-#===========Webcam==================
-import cv2
-
+# Mở webcam và hiển thị hình ảnh từ webcam
 frameWidth = 640
 frameHeight = 480
 cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-cap.set(10, 150)
+cap.set(3, frameWidth)  # Đặt chiều rộng của khung hình
+cap.set(4, frameHeight)  # Đặt chiều cao của khung hình
+cap.set(10, 150)  # Đặt độ sáng của khung hình
+
 while True:
     success, img = cap.read()
     cv2.imshow("Result", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Nhấn phím 'q' để thoát
         break
+
+cap.release()  # Giải phóng camera
+cv2.destroyAllWindows()  # Đóng tất cả cửa sổ hiển thị
